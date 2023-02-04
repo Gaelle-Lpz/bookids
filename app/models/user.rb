9d306e9a1a64603627b463ecbed88abe622ec2b6
Shaherazade Bendjelloun <shaherazadeb@gmail.com>
2023-02-04 14:58:58 +0100

    seed two

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :family
   has_many :families
   has_many :user_books
   has_many :books, through: :user_books
   has_many :user_rewards
+  has_many :rewards, through: :user_rewards
 end
