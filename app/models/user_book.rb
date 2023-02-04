075ce66b1b4b1b9225fd0588774d57852ddb187a
jessica coueron <jcoueron@yahoo.fr>
2023-02-04 14:11:16 +0100

    seed done

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,5 +1,6 @@
 class UserBook < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  enum :status, { wished: 0, pending: 1, read: 2 }, prefix: true, scopes: false
 end
