fcb93c2f0e09ba97dbac937090177e6682a3af07
jessica coueron <jcoueron@yahoo.fr>
2023-02-07 18:53:10 +0100

    reviews and link back to book lists modified

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,12 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :destroy]
   def index
     @books = Book.all
-    @reading_list_books = @books.select { |book| book.user_books.user == current_user && book.user_books.wish_validate }
-    @wish_list_books = @books.select { |book| book.user_books.user == current_user }
+    #@reading_list_books = @books.select { |book| book.user_books.user == current_user && book.user_books.wish_validate }
+    #@wish_list_books = @books.select { |book| book.user_books.user == current_user }
   end
 
   def show
+    @reviews = @book.reviews
   end
 
   def new
