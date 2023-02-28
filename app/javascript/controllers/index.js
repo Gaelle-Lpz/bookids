6d68b1ca0d6b963de0a49142f9040052c48f3d41
Shaherazade Bendjelloun <shaherazadeb@gmail.com>
2023-02-28 21:11:18 +0100

    confetti

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ConfettiController from "./confetti_controller"
+application.register("confetti", ConfettiController)
